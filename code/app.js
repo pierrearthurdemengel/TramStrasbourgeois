ce8b59e5bc176563b3f8da393ea726d2ff81f7bf
pierrearthurdemengel <pademengel@gmail.com>
2023-05-04 21:56:21 +0200

    Ok ne reste plus qu'à nettoyer l'id et changer les courleurs des arrets

--- code/app.js
+++ code/app.js
@@ -1,311 +1,380 @@
-$(function () {
-  // Définir allA... en dehors des fonctions anonymes
-  let allA = $("[id*=A_");
-  let allB = $("[id*=B_");
-  let allC = $("[id*=C_");
-  let allD = $("[id*=D_");
-  let allE = $("[id*=E_");
-  let allF = $("[id*=F_");
-  let allG = $("[id*=G_");
-
-  // Stocker une référence au dernier A cliqué
-  let lastA = null;
-  let lastB = null;
-  let lastC = null;
-  let lastD = null;
-  let lastE = null;
-  let lastF = null;
-  let lastG = null;
-  // A chaque click sur un A_     ///////////////////////////////////////////////////////////////////////////////
-  $("[id*=A_").on("click", function () {
-    // je selectionne l'arret A_ et je le stocke dans A
-    let A = $(this);
-
-    // je récupère son ID
-    let AId = $(this)["0"].id;
-
-    // Réinitialiser le dernier A cliqué
-    if (lastA != null) {
-      lastA.css("fill", "#fff");
-    }
-
-    // je les colorises en blanc
-    allB.css("fill", "#fff");
-    allC.css("fill", "#fff");
-    allD.css("fill", "#fff");
-    allE.css("fill", "#fff");
-    allF.css("fill", "#fff");
-    allG.css("fill", "#fff");
-
-    // je colore le A en rouge
-    A.css("fill", "rgb(255 0 0)");
-
-    // Stocker une référence au dernier cliqué
-    lastA = A;
-    
-
-    // afficher les arrets A bien écrit
-    AId = AId.replace("A_", "");
-
-    let boucle = true;
-
-    do {
-      AId = AId.replace("_", " ");
-      // prend la valeur de la position dans la chaine et si il n'en trouve pas il met -1
-      if (AId.indexOf("_") == -1) boucle = false;
-    } while (boucle);
-
-    $("#infosA").text(AId);
-    $("#infosB").text("");
-    $("#infosC").text("");
-    $("#infosD").text("");
-    $("#infosE").text("");
-    $("#infosF").text("");
-    $("#infosG").text("");
-
-    console.log(AId);
+$(function() {
+    // Définir les éléments à surveiller
+    const elements = ["A", "B", "C", "D", "E", "F", "G"];
+  
+    // Stocker une référence au dernier élément cliqué pour chaque type
+    const lastClicked = {};
+  
+    // Initialiser les éléments
+    const allElements = elements.reduce((acc, curr) => {
+      const el = $(`[id*="${curr}_"]`);
+      lastClicked[curr] = null;
+      return {...acc, [curr]: el };
+    }, {});
+  
+    // Définir la logique de gestion de clic pour chaque type d'élément
+    function handleClick(elementName, elementId) {
+        // Sélectionner l'élément et le stocker
+        const element = $(`#${elementId}`);
+        lastClicked[elementName] = element;
+      
+        // Réinitialiser le dernier élément cliqué
+        Object.values(allElements).forEach((el) => el.css("fill", "#fff"));
+      
+        // Colorier l'élément sélectionné en rouge si c'est A, vert si c'est D, orange si c'est C
+        if (elementName === "A") {
+          element.css("fill", "rgb(255, 0, 0)");
+        } else if (elementName === "B") {
+          element.css("fill", "rgb(255, 165, 0)");
+        } else if (elementName === "C") {
+          element.css("fill", "rgb(255, 165, 0)");
+        } else if (elementName === "D") {
+          element.css("fill", "rgb(0, 255, 0)");
+        } else if (elementName === "E") {
+          element.css("fill", "rgb(255, 165, 0)");
+        } else if (elementName === "F") {
+          element.css("fill", "rgb(255, 165, 0)");
+        } else if (elementName === "G") {
+          element.css("fill", "rgb(255, 165, 0)");
+        }
+      
+        // Extraire l'ID de l'élément et mettre à jour les informations textuelles
+        let id = elementId.replace(`${elementName}_`, "");
+        let done = false;
+      
+        while (!done) {
+          id = id.replace("_", " ");
+          done = id.indexOf("_") === -1;
+        }
+      
+        $(`#idCliqué`).text(elementId); // Afficher l'ID de l'élément cliqué
+      
+        // Envoyer une requête AJAX vers le fichier PHP avec l'ID comme données
+        let idToSend = elementId.replace(`${elementName}_`, "");
+        $.ajax({
+          url: "cts.php",
+          method: "POST",
+          data: { id: idToSend },
+          success: function(response) {
+            console.log(response);
+          }
+        });
+      };
+      
+  
+    // Ajouter les gestionnaires de clics à chaque élément
+    elements.forEach((el) => {
+      allElements[el].on("click", function (event) {
+        const elementId = this.id;
+        handleClick(el, elementId);
+      });
+    });
   });
 
-  // A chaque click sur un B_ //////////////////////////////////////////////////////////////////////////
-  $("[id*=B_").on("click", function () {
-    // je selectionne l'arret B_ et je le stocke dans B
-    let B = $(this);
+// $(function () {
+//   // Définir allA... en dehors des fonctions anonymes
+//   let allA = $("[id*=A_");
+//   let allB = $("[id*=B_");
+//   let allC = $("[id*=C_");
+//   let allD = $("[id*=D_");
+//   let allE = $("[id*=E_");
+//   let allF = $("[id*=F_");
+//   let allG = $("[id*=G_");
 
-    // je récupère son ID
-    let BId = $(this)["0"].id;
+//   // Stocker une référence au dernier A cliqué
+//   let lastA = null;
+//   let lastB = null;
+//   let lastC = null;
+//   let lastD = null;
+//   let lastE = null;
+//   let lastF = null;
+//   let lastG = null;
+//   // A chaque click sur un A_     ///////////////////////////////////////////////////////////////////////////////
+//   $("[id*=A_").on("click", function () {
+//     // je selectionne l'arret A_ et je le stocke dans A
+//     let A = $(this);
 
-    // Réinitialiser le dernier B cliqué
-    if (lastB != null) {
-      lastB.css("fill", "#fff");
-    }
+//     // je récupère son ID
+//     let AId = $(this)["0"].id;
 
-    // je les colorises en blanc
-    allA.css("fill", "#fff");
-    allC.css("fill", "#fff");
-    allD.css("fill", "#fff");
-    allE.css("fill", "#fff");
-    allF.css("fill", "#fff");
-    allG.css("fill", "#fff");
+//     // Réinitialiser le dernier A cliqué
+//     if (lastA != null) {
+//       lastA.css("fill", "#fff");
+//     }
 
-    // je colore le A en rouge
-    B.css("fill", "rgb(255 0 0)");
+//     // je les colorises en blanc
+//     allB.css("fill", "#fff");
+//     allC.css("fill", "#fff");
+//     allD.css("fill", "#fff");
+//     allE.css("fill", "#fff");
+//     allF.css("fill", "#fff");
+//     allG.css("fill", "#fff");
 
-    // Stocker une référence au dernier cliqué
-    lastB = B;
-    
+//     // je colore le A en rouge
+//     A.css("fill", "rgb(255 0 0)");
 
-    // afficher les arrets A bien écrit
-    BId = BId.replace("B_", "");
+//     // Stocker une référence au dernier cliqué
+//     lastA = A;
 
-    let boucle = true;
+//     // afficher les arrets A bien écrit
+//     AId = AId.replace("A_", "");
 
-    do {
-      BId = BId.replace("_", " ");
-      // prend la valeur de la position dans la chaine et si il n'en trouve pas il met -1
-      if (BId.indexOf("_") == -1) boucle = false;
-    } while (boucle);
+//     let boucle = true;
 
-    $("#infosA").text("");
-    $("#infosB").text(BId);
-    $("#infosC").text("");
-    $("#infosD").text("");
-    $("#infosE").text("");
-    $("#infosF").text("");
-    $("#infosG").text("");
+//     do {
+//       AId = AId.replace("_", " ");
+//       // prend la valeur de la position dans la chaine et si il n'en trouve pas il met -1
+//       if (AId.indexOf("_") == -1) boucle = false;
+//     } while (boucle);
 
-    console.log(BId);
-  });
+//     $("#infosA").text(AId);
+//     $("#infosB").text("");
+//     $("#infosC").text("");
+//     $("#infosD").text("");
+//     $("#infosE").text("");
+//     $("#infosF").text("");
+//     $("#infosG").text("");
 
-  // A chaque click sur un C_ //////////////////////////////////////////////////////////////////////////////////////////////////
-  $("[id*=C_").on("click", function () {
-    // je selectionne l'arret C_ et je le stocke dans C
-    let C = $(this);
+//     console.log(AId);
+//   });
 
-    // je récupère son ID
-    let CId = $(this)["0"].id;
+//   // A chaque click sur un B_ //////////////////////////////////////////////////////////////////////////
+//   $("[id*=B_").on("click", function () {
+//     // je selectionne l'arret B_ et je le stocke dans B
+//     let B = $(this);
 
-    // Réinitialiser le dernier C cliqué
-    if (lastC != null) {
-      lastC.css("fill", "#fff");
-    }
+//     // je récupère son ID
+//     let BId = $(this)["0"].id;
 
-    // je les colorises en Clanc
-    allA.css("fill", "#fff");
-    allB.css("fill", "#fff");
-    allD.css("fill", "#fff");
-    allE.css("fill", "#fff");
-    allF.css("fill", "#fff");
-    allG.css("fill", "#fff");
+//     // Réinitialiser le dernier B cliqué
+//     if (lastB != null) {
+//       lastB.css("fill", "#fff");
+//     }
 
-    // je colore le A en rouge
-    C.css("fill", "rgb(255 0 0)");
+//     // je les colorises en blanc
+//     allA.css("fill", "#fff");
+//     allC.css("fill", "#fff");
+//     allD.css("fill", "#fff");
+//     allE.css("fill", "#fff");
+//     allF.css("fill", "#fff");
+//     allG.css("fill", "#fff");
 
-    // Stocker une référence au dernier cliqué
-    lastC = C;
-    
+//     // je colore le A en rouge
+//     B.css("fill", "rgb(255 0 0)");
 
-    // afficher les arrets A bien écrit
-    CId = CId.replace("C_", "");
+//     // Stocker une référence au dernier cliqué
+//     lastB = B;
 
-    let boucle = true;
+//     // afficher les arrets A bien écrit
+//     BId = BId.replace("B_", "");
 
-    do {
-      CId = CId.replace("_", " ");
-      // prend la valeur de la position dans la chaine et si il n'en trouve pas il met -1
-      if (CId.indexOf("_") == -1) boucle = false;
-    } while (boucle);
+//     let boucle = true;
 
-    $("#infosA").text("");
-    $("#infosB").text("");
-    $("#infosC").text(CId);
-    $("#infosD").text("");
-    $("#infosE").text("");
-    $("#infosF").text("");
-    $("#infosG").text("");
+//     do {
+//       BId = BId.replace("_", " ");
+//       // prend la valeur de la position dans la chaine et si il n'en trouve pas il met -1
+//       if (BId.indexOf("_") == -1) boucle = false;
+//     } while (boucle);
 
-    console.log(CId);
-  });
+//     $("#infosA").text("");
+//     $("#infosB").text(BId);
+//     $("#infosC").text("");
+//     $("#infosD").text("");
+//     $("#infosE").text("");
+//     $("#infosF").text("");
+//     $("#infosG").text("");
 
-  // A chaque click sur un D_ /////////////////////////////////////////////////////////////////////////////////////////////////
-  $("[id*=D_").on("click", function () {
-    // je selectionne l'arret D_ et je le stocke dans D
-    let D = $(this);
+//     console.log(BId);
+//   });
 
-    // je récupère son ID
-    let DId = $(this)["0"].id;
+//   // A chaque click sur un C_ //////////////////////////////////////////////////////////////////////////////////////////////////
+//   $("[id*=C_").on("click", function () {
+//     // je selectionne l'arret C_ et je le stocke dans C
+//     let C = $(this);
 
-    // Réinitialiser le dernier D cliqué
-    if (lastD != null) {
-      lastD.css("fill", "#fff");
-    }
+//     // je récupère son ID
+//     let CId = $(this)["0"].id;
 
-    // je les colorises en Clanc
-    allA.css("fill", "#fff");
-    allB.css("fill", "#fff");
-    allC.css("fill", "#fff");
-    allE.css("fill", "#fff");
-    allF.css("fill", "#fff");
-    allG.css("fill", "#fff");
+//     // Réinitialiser le dernier C cliqué
+//     if (lastC != null) {
+//       lastC.css("fill", "#fff");
+//     }
 
-    // je colore le D en rouge
-    D.css("fill", "rgb(255 0 0)");
+//     // je les colorises en Clanc
+//     allA.css("fill", "#fff");
+//     allB.css("fill", "#fff");
+//     allD.css("fill", "#fff");
+//     allE.css("fill", "#fff");
+//     allF.css("fill", "#fff");
+//     allG.css("fill", "#fff");
 
-    // Stocker une référence au dernier cliqué
-    lastD = D;
-    
+//     // je colore le A en rouge
+//     C.css("fill", "rgb(255 0 0)");
 
-    // afficher les arrets D bien écrit
-    DId = DId.replace("A_", "");
+//     // Stocker une référence au dernier cliqué
+//     lastC = C;
 
-    let boucle = true;
+//     // afficher les arrets A bien écrit
+//     CId = CId.replace("C_", "");
 
-    do {
-      DId = DId.replace("_", " ");
-      // prend la valeur de la position dans la chaine et si il n'en trouve pas il met -1
-      if (DId.indexOf("_") == -1) boucle = false;
-    } while (boucle);
+//     let boucle = true;
 
-    $("#infosA").text("");
-    $("#infosB").text("");
-    $("#infosC").text(CId);
-    $("#infosD").text("");
-    $("#infosE").text("");
-    $("#infosF").text("");
-    $("#infosG").text("");
+//     do {
+//       CId = CId.replace("_", " ");
+//       // prend la valeur de la position dans la chaine et si il n'en trouve pas il met -1
+//       if (CId.indexOf("_") == -1) boucle = false;
+//     } while (boucle);
 
-    console.log(DId);
-  });
-  // A chaque click sur un E_  //////////////////////////////////////////////////////////////////////////
-  $("[id*=E_]").on("click", function () {
-    let E = $(this);
-    let EId = $(this)["0"].id;
+//     $("#infosA").text("");
+//     $("#infosB").text("");
+//     $("#infosC").text(CId);
+//     $("#infosD").text("");
+//     $("#infosE").text("");
+//     $("#infosF").text("");
+//     $("#infosG").text("");
 
-    allA.css("fill", "#fff");
-    allB.css("fill", "#fff");
-    allC.css("fill", "#fff");
-    allD.css("fill", "#fff");
-    allF.css("fill", "#fff");
-    allG.css("fill", "#fff");
-    E.css("fill", "rgba(0, 0, 255, 0.4)");
+//     console.log(CId);
+//   });
 
-    EId = EId.replace("E_", "");
-    let boucle = true;
-    do {
-      EId = EId.replace("_", " ");
-      if (EId.indexOf("_") == -1) boucle = false;
-    } while (boucle);
+//   // A chaque click sur un D_ /////////////////////////////////////////////////////////////////////////////////////////////////
+//   $("[id*=D_").on("click", function () {
+//     // je selectionne l'arret D_ et je le stocke dans D
+//     let D = $(this);
 
-    $("#infosA").text("");
-    $("#infosB").text("");
-    $("#infosC").text("");
-    $("#infosD").text("");
-    $("#infosE").text("");
-    $("#infosF").text("");
-    $("#infosG").text("");
-    $("#infosE").text(GId);
+//     // je récupère son ID
+//     let DId = $(this)["0"].id;
 
-    console.log(EId);
-  });
+//     // Réinitialiser le dernier D cliqué
+//     if (lastD != null) {
+//       lastD.css("fill", "#fff");
+//     }
 
-  // A chaque click sur un F_  //////////////////////////////////////////////////////////////////////////
-  $("[id*=F_]").on("click", function () {
-    let F = $(this);
-    let FId = $(this)["0"].id;
+//     // je les colorises en Clanc
+//     allA.css("fill", "#fff");
+//     allB.css("fill", "#fff");
+//     allC.css("fill", "#fff");
+//     allE.css("fill", "#fff");
+//     allF.css("fill", "#fff");
+//     allG.css("fill", "#fff");
 
-    allA.css("fill", "#fff");
-    allB.css("fill", "#fff");
-    allC.css("fill", "#fff");
-    allD.css("fill", "#fff");
-    allE.css("fill", "#fff");
-    allG.css("fill", "#fff");
-    F.css("fill", "rgba(0, 143, 0, 0.7)");
+//     // je colore le D en rouge
+//     D.css("fill", "rgb(255 0 0)");
 
-    FId = FId.replace("F_", "");
-    let boucle = true;
-    do {
-      FId = FId.replace("_", " ");
-      if (FId.indexOf("_") == -1) boucle = false;
-    } while (boucle);
+//     // Stocker une référence au dernier cliqué
+//     lastD = D;
 
-    $("#infosA").text("");
-    $("#infosB").text("");
-    $("#infosC").text("");
-    $("#infosD").text("");
-    $("#infosE").text("");
-    $("#infosF").text(FId);
-    $("#infosG").text("");
+//     // afficher les arrets D bien écrit
+//     DId = DId.replace("A_", "");
 
-    console.log(FId);
-  });
+//     let boucle = true;
 
-  // A chaque click sur un G_  //////////////////////////////////////////////////////////////////////////
-  $("[id*=G_]").on("click", function () {
-    let G = $(this);
-    let GId = $(this)["0"].id;
+//     do {
+//       DId = DId.replace("_", " ");
+//       // prend la valeur de la position dans la chaine et si il n'en trouve pas il met -1
+//       if (DId.indexOf("_") == -1) boucle = false;
+//     } while (boucle);
 
-    allA.css("fill", "#fff");
-    allB.css("fill", "#fff");
-    allC.css("fill", "#fff");
-    allD.css("fill", "#fff");
-    allE.css("fill", "#fff");
-    allF.css("fill", "#fff");
-    G.css("fill", "rgb(0, 0, 255)");
+//     $("#infosA").text("");
+//     $("#infosB").text("");
+//     $("#infosC").text(CId);
+//     $("#infosD").text("");
+//     $("#infosE").text("");
+//     $("#infosF").text("");
+//     $("#infosG").text("");
 
-    GId = GId.replace("G_", "");
-    let boucle = true;
-    do {
-      GId = GId.replace("_", " ");
-      if (GId.indexOf("_") == -1) boucle = false;
-    } while (boucle);
+//     console.log(DId);
+//   });
+//   // A chaque click sur un E_  //////////////////////////////////////////////////////////////////////////
+//   $("[id*=E_]").on("click", function () {
+//     let E = $(this);
+//     let EId = $(this)["0"].id;
 
-    $("#infosA").text("");
-    $("#infosB").text("");
-    $("#infosC").text("");
-    $("#infosD").text("");
-    $("#infosE").text("");
-    $("#infosF").text("");
-    $("#infosG").text(GId);
+//     allA.css("fill", "#fff");
+//     allB.css("fill", "#fff");
+//     allC.css("fill", "#fff");
+//     allD.css("fill", "#fff");
+//     allF.css("fill", "#fff");
+//     allG.css("fill", "#fff");
+//     E.css("fill", "rgba(0, 0, 255, 0.4)");
 
-    console.log(FId);
-  });
-});
+//     EId = EId.replace("E_", "");
+//     let boucle = true;
+//     do {
+//       EId = EId.replace("_", " ");
+//       if (EId.indexOf("_") == -1) boucle = false;
+//     } while (boucle);
+
+//     $("#infosA").text("");
+//     $("#infosB").text("");
+//     $("#infosC").text("");
+//     $("#infosD").text("");
+//     $("#infosE").text("");
+//     $("#infosF").text("");
+//     $("#infosG").text("");
+//     $("#infosE").text(GId);
+
+//     console.log(EId);
+//   });
+
+//   // A chaque click sur un F_  //////////////////////////////////////////////////////////////////////////
+//   $("[id*=F_]").on("click", function () {
+//     let F = $(this);
+//     let FId = $(this)["0"].id;
+
+//     allA.css("fill", "#fff");
+//     allB.css("fill", "#fff");
+//     allC.css("fill", "#fff");
+//     allD.css("fill", "#fff");
+//     allE.css("fill", "#fff");
+//     allG.css("fill", "#fff");
+//     F.css("fill", "rgba(0, 143, 0, 0.7)");
+
+//     FId = FId.replace("F_", "");
+//     let boucle = true;
+//     do {
+//       FId = FId.replace("_", " ");
+//       if (FId.indexOf("_") == -1) boucle = false;
+//     } while (boucle);
+
+//     $("#infosA").text("");
+//     $("#infosB").text("");
+//     $("#infosC").text("");
+//     $("#infosD").text("");
+//     $("#infosE").text("");
+//     $("#infosF").text(FId);
+//     $("#infosG").text("");
+
+//     console.log(FId);
+//   });
+
+//   // A chaque click sur un G_  //////////////////////////////////////////////////////////////////////////
+//   $("[id*=G_]").on("click", function () {
+//     let G = $(this);
+//     let GId = $(this)["0"].id;
+
+//     allA.css("fill", "#fff");
+//     allB.css("fill", "#fff");
+//     allC.css("fill", "#fff");
+//     allD.css("fill", "#fff");
+//     allE.css("fill", "#fff");
+//     allF.css("fill", "#fff");
+//     G.css("fill", "rgb(0, 0, 255)");
+
+//     GId = GId.replace("G_", "");
+//     let boucle = true;
+//     do {
+//       GId = GId.replace("_", " ");
+//       if (GId.indexOf("_") == -1) boucle = false;
+//     } while (boucle);
+
+//     $("#infosA").text("");
+//     $("#infosB").text("");
+//     $("#infosC").text("");
+//     $("#infosD").text("");
+//     $("#infosE").text("");
+//     $("#infosF").text("");
+//     $("#infosG").text(GId);
+
+//     console.log(FId);
+//   });
+// });
--- code/cts.php
+++ code/cts.php
@@ -1,38 +1,50 @@
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Carte CTS</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="style.css">
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-        <!-- <script> src="https:code.jquery.com/jquery.3.2.1.min.js" type="text/javascript"</script> -->
-        <script type="text/javascript" src="app.js"></script>
-    </head>
-    <body>
 
-        <div class="mapAndInfos">
-            <div class="map">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Carte CTS</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <!-- <script> src="https:code.jquery.com/jquery.3.2.1.min.js" type="text/javascript"</script> -->
+    <script type="text/javascript" src="app.js"></script>
+</head>
 
-                <?php include('arrets.svg'); ?>
+<body>
 
-            </div>
+    <div class="mapAndInfos">
+        <div class="map">
 
-            <div class="info">
-                <h1>Carte CTS</h1>
+            <?php include('arrets.svg'); ?>
 
-                <span id="infosA"></span>
+        </div>
+
+        <div class="info">
+    <h1>Carte CTS</h1>
+    <?php
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        echo "<h1>$id</h1>";
+    }
+    
+    ?>
+    <span id="idCliqué"></span>
+
+
+            <!-- <span id="infosA"></span>
                 <span id="infosB"></span> 
                 <span id="infosB"></span>
                 <span id="infosD"></span> 
                 <span id="infosE"></span>
                 <span id="infosF"></span> 
-                <span id="infosG"></span>
-        
+                <span id="infosG"></span> -->
+
         </div>
-        
-    </body>
+
+</body>
+
 </html>
